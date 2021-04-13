680119ef1eb81f3613777452bc99b86fed3a7441
Ritaayu <ritaygtys@students.amikom.ac.id>
2021-04-13 12:31:49 +0700

    responsi pemograman

---
+++ Pemograman3306.cs
@@ -0,0 +1,27 @@
+
+namespace ConsoleApp1
+{
+    class Responsi
+    {
+        static void Main(string[]args)
+        {
+            int NIK;
+            String Nama;
+            int Karyawan1;
+            int Karyawan2;
+            int gaji1 = 3000;
+            int gaji2 = 3000;
+            Console.WriteLine("==========================");
+            Console.WriteLine("NIK= "); NIK = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nama= "); Nama = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("==========================");
+
+            Karyawan1 = gaji1;
+            Karyawan2 = gaji2;
+
+            Console.WriteLine("Selamat anda mendapatkan bonus Gaji 10%");
+        }
+    }
+}
+
+		
